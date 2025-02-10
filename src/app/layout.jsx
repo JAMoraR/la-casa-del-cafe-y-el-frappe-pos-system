24cdef90e7b290fe5ccaba911656f542161deeaf
Jamorar <capi.miguel10@gmail.com>
2025-02-09 20:35:25 -0500

    * Metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,8 @@
 import "@/styles/globals.css";
 
 export const metadata = {
-  title: "Administración | La casa del Café y el Frappe",
-  description: "Administración de la casa del Café y el Frappe",
+  title: "Venta | La casa del Café y el Frappe",
+  description: "Punto de venta de La casa del Café y el Frappe",
 };
 
 export default function RootLayout({ children }) {
